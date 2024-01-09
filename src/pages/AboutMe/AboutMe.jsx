643aba0900b5bab64c0879cf5fa3e60453e73195
tanvir <tanvir0046@gmail.com>
2024-01-09 18:37:23 +0600

    home section work is done

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import TextTitle from '../../hooks/TextTitle'
+// import video from '../../assets/network_-_27018 (Original).mp4'
 // import { FaAngleDoubleUp } from "react-icons/fa";
 // import { Link } from "react-scroll";
 
@@ -11,9 +12,10 @@ const Section = styled.div`
 
 const AboutMe = () => {
     return (
-        <Section id="about" className="px-4 md:px-6 lg:px-12 mx-auto">
+        <Section id="about" className="">
             <TextTitle
                 header='about' />
+
         </Section>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,55 +1,62 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/no-unescaped-entities */
 import styled from "styled-components";
-// import { useTypewriter } from 'react-simple-typewriter'
 import { Typewriter } from 'react-simple-typewriter'
 import Navbar from '../../Components/Navbar/Navbar'
-import video from '../../assets/network_-_27018 (Original).mp4'
 import './Home'
+import { MeshDistortMaterial, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Sphere } from "@react-three/drei";
 
 const Section = styled.div`
-    height: 100vh;
     scroll-snap-align: center;
 `
 
 const Home = () => {
 
-
-
     return (
-        <Section id="home" className="">
-            <div className="object-cover w-full relative">
-                <video src={video} autoPlay loop muted />
-                <div className="absolute w-[100%] top-0 left-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.8)]">
-                    <div className="fixed">
-                        <Navbar />
+        <Section id="home" className="h-full">
+            <div className="fixed">
+                <Navbar />
+            </div>
+            <div className="lg:min-h-screen w-full flex justify-center items-center">
+                <div className="hero-content flex-col lg:flex-row-reverse w-3/4 h-4/5 mt-10 lg:mt-0">
+                    <div className="flex-1 flex justify-center items-center h-4/5 relative">
+                        <Canvas style={{ height: '400px' }}>
+                            <OrbitControls enableZoom={false} autoRotate />
+                            <ambientLight intensity={1} />
+                            <directionalLight position={[3, 2, 1]} />
+                            <Sphere args={[1, 100, 200]} scale={2.5}>
+                                <MeshDistortMaterial
+                                    color={'#3d3085'}
+                                    attach='material'
+                                    distort={0.5}
+                                    speed={2}
+                                />
+                            </Sphere>
+                        </Canvas>
+                        <div className="z-50 absolute mr-10">
+                            <img src="https://i.ibb.co/tZHWnC1/1700749319053.png" className="max-w-sm" />
+                        </div>
                     </div>
-                    <div className="min-h-screen flex justify-center items-center">
-                        <div className="hero-content flex-col lg:flex-row-reverse w-3/4">
-                            <div className="flex-1 flex justify-center items-center">
-                                <div>
-                                    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-                                </div>
-                            </div>
-                            <div className="flex-1 flex">
-                                <div>
-                                    <h1 className="text-white mb-2"><span className="text-4xl" id="font">Hello! I'M</span> <br />
-                                        <span className="text-5xl">MD.MIRAJUL ISLAM</span></h1>
-                                    <h1 className="text-xl text-white mt-3">
-                                        <span className="text-emerald-500 text-3xl">
-                                            <Typewriter words={['FONT END DEVELOPER', 'MERN STACK DEVELOPER', 'REACT DEVELOPER']}
-                                                loop={5}
-                                                cursor
-                                                cursorStyle='|'
-                                                typeSpeed={70}
-                                                deleteSpeed={50}
-                                                delaySpeed={1000} />
-                                        </span>
-                                        <br />
-                                        <span className="text-3xl"></span>
-                                    </h1>
-                                    <button className="btn btn-primary mt-5">Get Started</button>
-                                </div>
-                            </div>
+                    <div className="flex-1 flex">
+                        <div>
+                            <h1 className="text-white mb-2"><span className="text-4xl" id="font">Hello! I'M</span> <br />
+                                <span className="text-5xl">MD.MIRAJUL ISLAM</span></h1>
+                            <h1 className="text-xl text-white mt-3">
+                                <span className="text-emerald-500 text-3xl">
+                                    <Typewriter words={['FONT END DEVELOPER', 'MERN STACK DEVELOPER', 'REACT DEVELOPER']}
+                                        loop={5}
+                                        cursor
+                                        cursorStyle='|'
+                                        typeSpeed={70}
+                                        deleteSpeed={50}
+                                        delaySpeed={1000} />
+                                </span>
+                                <br />
+                                <span className="text-3xl"></span>
+                            </h1>
+                            <button className="btn btn-primary mt-5">Get Started</button>
                         </div>
                     </div>
                 </div>
